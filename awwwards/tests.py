47a5c244971540491534fa5007006ed0423f2bae
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-11 18:21:35 +0300

    run save & delete profile unit tests, passed

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -7,3 +7,14 @@ class ProfileTestClass(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.nancy,Profile))    
+
+    def test_save_method(self):
+        self.nancy.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.nancy.save_profile()
+        self.nancy.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)    
