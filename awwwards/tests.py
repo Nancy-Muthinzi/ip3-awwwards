b1a50a2a72d050803c260ef0e3ced1e84684bd98
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-11 17:13:46 +0300

    define unit tests, write first test, passed

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Profile, Project
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.nancy = Profile(name = 'Nancy', email = 'kathinimuthinzi@gmail.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nancy,Profile))    
