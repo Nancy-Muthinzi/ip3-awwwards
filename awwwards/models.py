ac55027797b57f414ad4c192866289a845952e75
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-11 16:49:41 +0300

    create profile model

--- awwwards/models.py
+++ awwwards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length = 25, default = "Nancy Muthinzi")
+    email = models.EmailField()
+   
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
