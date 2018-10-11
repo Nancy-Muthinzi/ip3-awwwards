f626a1cbcb240110c91c7a75f12fd989f9332d75
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-11 18:34:05 +0300

    create superuser

--- awwwards/tests.py
+++ awwwards/tests.py
@@ -17,4 +17,30 @@ class ProfileTestClass(TestCase):
         self.nancy.save_profile()
         self.nancy.delete_profile()
         profiles = Profile.objects.all()
-        self.assertTrue(len(profiles) == 0)    
+        self.assertTrue(len(profiles) == 0)   
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.nancy = Profile(name = 'Nancy', email = 'kathinimuthinzi@gmail.com')
+        self.nancy.save_profile()
+
+    def setUp(self):
+        self.new_project = Project(title = 'Test Project', post = 'This is a random test post', profile = self.nancy)
+        self.new_project.save()  
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Project.objects.all().delete()    
+
+    def test_save_method(self):
+        self.test_project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)  
+
+    def test_delete_method(self):
+        self.test_project.save_project()
+        self.nancy.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0) 
+
+
