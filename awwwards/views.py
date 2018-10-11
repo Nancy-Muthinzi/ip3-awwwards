de71025a56ab358afa0ef6123a2bdd1b02ee28f4
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-11 19:37:06 +0300

    import project model, create form class for new project

--- awwwards/views.py
+++ awwwards/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from .models import Project
 from django.contrib.auth.decorators import login_required
+from .forms import NewProjectForm
 
 @login_required(login_url='/accounts/login/')
 def home(request):
     
     return render(request, 'home.html')
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit = False)
+            project.profile = current_user
+            project.save()
+        return redirect('Home')    
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form":form})        
+
+
+
