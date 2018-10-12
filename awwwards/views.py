c0cf7f72edf6b6c34cefd2dafa403eff5ac0c4bc
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-12 08:28:24 +0300

    link log in and sign up templates to sidenav

--- awwwards/views.py
+++ awwwards/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Project
 from django.contrib.auth.decorators import login_required
 from .forms import NewProjectForm, NewsLetterForm
 from .email import send_welcome_email
+
 
 @login_required(login_url='/accounts/login/')
 def home(request):
@@ -13,12 +14,14 @@ def home(request):
             name = form.cleaned_data['your_name']
             email = form.cleaned_data['email']
 
-            recipient = NewsLetterRecipients(name = name,email =email)
+            recipient = NewsLetterRecipients(name=name, email=email)
             recipient.save()
-            send_welcome_email(name,email)
+            send_welcome_email(name, email)
 
-    
+            HttpResponseRedirect('home', {"news": news, "letterForm": form})
+
     return render(request, 'home.html')
+
 
 @login_required(login_url='/accounts/login/')
 def new_project(request):
@@ -27,14 +30,11 @@ def new_project(request):
     if request.method == 'POST':
         form = NewProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            project = form.save(commit = False)
+            project = form.save(commit=False)
             project.profile = current_user
             project.save()
-        return redirect('Home')    
+        return redirect('Home')
 
     else:
         form = NewProjectForm()
-    return render(request, 'new_project.html', {"form":form})        
-
-
-
+    return render(request, 'new_project.html', {"form": form})
