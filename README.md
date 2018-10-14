1950b85432cfc9669e17febd05a4a41f3d18c785
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-14 16:28:07 +0300

    create Procfile, runtime files

---
+++ README.md
@@ -0,0 +1,49 @@
+# AWWWARDS
+
+<img src="/awwwards/static/images/awww.png">
+
+This is a website that allows a user to post his/her projects and other users to rate them according to design, usability and content.
+
+#### A user of the application will be able to:
+
+- View posted projects and their details.
+- Post a project to be rated.
+- Rate other users' projects.
+- Search for projects.
+- View his/her profile page.
+
+## Setup/Installation Requirements
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes:
+
+- go to my repo on github and clone it - https://github.com/Nancy-Muthinzi/ip3-awwwards.git
+- create a folder on your computer, create and activate a virtual environment
+- download django and install all the necessary requirements to be able to run the app
+- finally do a ./manage.py runserver to be able to run it on your browser
+
+Here's a link to the deployed site on Heroku - https://nancymuthinzi-awwwards.herokuapp.com/
+
+### Technologies used
+
+- Python3.6
+- Django 1.11
+- HTML
+- CSS
+- Bootstrap3
+
+## Known Bugs, Support & Contact Information
+
+This is still a work in progress and will continue to be worked on. But for any issues encountered and any contributions to the code or design, kindly reach out to me on my email address: nanciekathini@gmail.com.
+
+### Authors
+
+Nancy Muthinzi
+
+### License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+### Acknowledgments/Credits
+
+- Hat tip to Moringa School staff, technical mentors and fellow students for their support and guidance
+- StackOverflow, W3Schools, Codepen and other websites for their documentation and useful information
--- ip3/settings.py
+++ ip3/settings.py
@@ -11,7 +11,38 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
+import django_heroku
+import dj_database_url
+from decouple import config, Csv
+
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +57,7 @@ SECRET_KEY = 'jlb$w)4fe%n6cd@e$2y5tnp2x1+y%as3&)r3fcb%#a1!5qly4u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nancymuthinzi.awwwards.com', '127.0.0.1']
 
 
 # Application definition
@@ -35,18 +66,21 @@ INSTALLED_APPS = [
     'awwwards.apps.AwwwardsConfig',
     'bootstrap3',
     'star_ratings',
+    'registration',
     'tinymce',
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.messages'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,13 +160,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL='home'
 
 # Email configurations
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
@@ -140,3 +178,6 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
